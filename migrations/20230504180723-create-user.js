c73f3ba74e4651dbab1a93d0bd3eb0dd3b71bd8f
hpipou <mr.aoudjit@gmail.com>
2023-05-04 20:14:40 +0200

    INITIALISATION DES BDD AVEC SEQUELIZE

---
+++ migrations/20230504180723-create-user.js
@@ -0,0 +1,52 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        primaryKey: true,
+        type: Sequelize.UUID
+      },
+      username: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      role: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      accountStatus: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      freeSpace: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      usedSpace: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
